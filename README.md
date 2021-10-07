0c59c10945d8468eb789be5ae04de681c1788e13
Atul Acharya <atul@Atuls-MacBook-Air.local>
2021-10-07 10:55:46 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# PhotoShare-App
+
+This is a MERN stack based Web Application where users can share and like photos.
+
+It supports the following features
+
+Google OAuth login
+Email based login
+Sharing photos, tagging others in the photos, liking others' shared photos, commenting on photos, deleting shared photos.
+Search photos by tags.
+Setup:
+
+run "npm i" && "npm start" for both client and server side to start the app
---
+++ server/index.js
@@ -0,0 +1,30 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
+
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}photoshare2021@cluster0.9vfpg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+//const CONNECTION_URL = 'mongodb+srv://js_mastery:M6WfDnJEoj9HkV2d@practice.jto9p.mongodb.net/memories_app?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
